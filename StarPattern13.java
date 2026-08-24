03d0817495653c048f9a995ff7f79fd631fa3b32
Amit Rahul Manwar <amitmanwar001@gmail.com>
2026-08-24 23:42:06 +0530

    Add hollow diamond star pattern

---
+++ StarPattern13.java
@@ -0,0 +1,33 @@
+public class StarPattern13 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int column = 1; column <= 2 * row - 1; column++) {
+                if (row == n || column == 1 || column == 2 * row - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int row = n - 1; row >= 1; row--) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print(" ");
+            }
+            for (int column = 1; column <= 2 * row - 1; column++) {
+                if (column == 1 || column == 2 * row - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
