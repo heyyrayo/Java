58b3b44f9d65b90cc375634cd23fd3b232e44502
heyyrayo <amitmanwar001@gmail.com>
2026-08-25 15:07:54 +0530

    Clean up Star3 formatting

--- Star3.java
+++ Star3.java
@@ -1,6 +1,6 @@
 public class Star3 {
     public static void main(String[] args) {
-        int n = 5; 
+        int n = 5;
 
         for (int i = 1; i <= n; i++) {
             for (int j = n; j > i; j--) {
