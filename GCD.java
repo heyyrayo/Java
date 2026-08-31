7c51a405e0d34c7671a93bfb30a90d186395c6df
heyyrayo <amitmanwar001@gmail.com>
2026-08-31 14:42:40 +0530

    Add GCD interview program

---
+++ GCD.java
@@ -0,0 +1,14 @@
+public class GCD {
+    public static void main(String[] args) {
+        int a = 48;
+        int b = 18;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD of 48 and 18 is: " + a);
+    }
+}
